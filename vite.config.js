383cd7686deae8d77321db43e1760fdc62effff5
Dan Parsley <dp846@Dans-MacBook-Air.local>
2026-07-01 23:54:31 +0100

    fix: set base path to /PersonalWebsite/ when building on GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/PersonalWebsite/' : '/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
